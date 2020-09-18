bdb8667fd8101964b908c73814413bbe8e2f9ce7
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2020-09-18 07:29:12 -0400

    feat(doublyLinkedList): create insertAt method

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -43,6 +43,32 @@ class DoublyLinkedList {
     }
   }
 
+  insertAt(pos, x) {
+    const newNode = new Node(x);
+
+    if (pos === 0) {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    let curr = this.head;
+    let index = 0;
+
+    while(curr) {
+      if (index === pos) {
+        curr.prev.next = newNode;
+        newNode.next = curr;
+        newNode.prev = curr.prev;
+        curr.prev = newNode;
+        return this.head;
+      }
+
+      curr = curr.next;
+      index++;
+    }
+  }
+
   deleteVal(val) {
     if (this.head && this.head.val === val) {
       if (this.head.next) {
@@ -78,8 +104,9 @@ d.append(1);
 d.append(2);
 d.append(3);
 d.append(4);
+d.insertAt(3, 100);
 
-d.deleteVal(2);
+// d.deleteVal(2);
 // d.deleteVal(1);
 
 d.print();
