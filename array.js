7306825c2ba849bebe9a6b4375b22e367e917062
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2020-10-03 17:03:06 -0400

    feat(array): find first and last position of element in sorted array using binary search

--- array.js
+++ array.js
@@ -19,4 +19,76 @@ function twoSum(nums, target) {
       return [sumMap[curr], i];
     }
   }
+}
+
+/*
+ Find First and Last Position of Element in Sorted Array
+
+ Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+If the target is not found in the array, return [-1, -1].
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+*/
+
+function searchRange(nums, target) {
+  if (nums.length == 1) {
+    if (nums[0] == target) return [0, 0];
+    return [-1, -1];
+  }
+
+  if (nums[0] == target && nums[nums.length - 1] == target) {
+    return [0, nums.length - 1];
+  }
+
+  const foundAt = binarySearch(nums, target);
+
+  if (foundAt == -1) {
+    return [-1, -1];
+  }
+
+  let first = foundAt;
+  let second = foundAt;
+
+  if (nums[foundAt - 1] == target) {
+    while (nums[first - 1] == target) {
+      first--;
+    }
+  }
+
+  if (nums[foundAt + 1] == target) {
+    while (nums[second + 1] == target) {
+      second++;
+    }
+  }
+
+  return [first, second];
 };
+
+function binarySearch(nums, target) {
+  let low = 0;
+  let high = nums.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (nums[mid] == target) {
+      return mid;
+    } else if (target < nums[mid]) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return -1;
+}
