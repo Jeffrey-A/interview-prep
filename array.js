efa98618677969e44b49e8c71612f225f0c37f6b
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2020-10-07 08:13:03 -0400

    feat(array): solve sort-colors problem

--- array.js
+++ array.js
@@ -179,3 +179,57 @@ function firstMissingPositive(nums) {
 
   return nums.length + 1;
 }
+
+/*
+Sort Colors
+
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+Example 3:
+
+Input: nums = [0]
+Output: [0]
+
+
+
+
+*/
+
+function sortColors(nums) {
+  if (nums.length == 1) return nums;
+
+  let first = 0;
+  let curr = 0;
+  let last = nums.length - 1;
+
+  while (curr <= last) {
+    if (nums[curr] == 0) {
+      swap(nums, first, curr);
+      first++;
+      curr++;
+    } else if (nums[curr] == 1) {
+      curr++;
+    } else if (nums[curr] == 2) {
+      swap(nums, last, curr);
+      last--;
+    }
+  }
+
+  return nums;
+};
+
+function swap(nums, pos1, pos2) {
+  let temp = nums[pos1];
+  nums[pos1] = nums[pos2];
+  nums[pos2] = temp;
+}
