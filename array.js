54f9ec97fc212b301429281a56499ba4ccbd5d1d
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2020-10-07 22:13:32 -0400

    feat(array): add brute force solution to single-number problem

--- array.js
+++ array.js
@@ -226,10 +226,38 @@ function sortColors(nums) {
   }
 
   return nums;
-};
+}
 
 function swap(nums, pos1, pos2) {
   let temp = nums[pos1];
   nums[pos1] = nums[pos2];
   nums[pos2] = temp;
+}
+
+/* 
+Single Number
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
+*/
+
+function singleNumber(nums) {
+  const occ = {};
+
+  nums.forEach((num) => {
+    if (!occ[num]) {
+      occ[num] = 1;
+    } else {
+      occ[num] = occ[num] + 1;
+    }
+  });
+
+  const ans = Object.keys(occ).filter((key) => occ[key] == 1);
+
+  if (ans.length) {
+    return ans[0];
+  }
+
+  return -1;
 }
