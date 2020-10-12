559585966ba36e58ba30e55132d02b69512eca33
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2020-10-12 08:30:50 -0400

    feat(array): solve course schedule problem using topological sort algo

--- array.js
+++ array.js
@@ -294,3 +294,69 @@ result => [[5,0], [7,0], [5,2], [6,1], [7,1]]
 result => [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
 
 */
+
+/*
+Course Schedule
+
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses-1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+ 
+
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0. So it is possible.
+Example 2:
+
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0, and to take course 0 you should
+             also have finished course 1. So it is impossible.
+
+*/
+
+var canFinish = function (numCourses, prerequisites) {
+  const graph = {};
+
+  let count = 0;
+
+  for (let i = 0; i < numCourses; i++) {
+    graph[i] = 0;
+  }
+
+  // create a grath representation with in-degree for each vertex
+  for (let i = 0; i < prerequisites.length; i++) {
+    graph[prerequisites[i][0]] = graph[prerequisites[i][0]] + 1;
+  }
+
+  const stack = [];
+  for (let i = 0; i < Object.keys(graph).length; i++) {
+    if (graph[i] == 0) {
+      stack.push(i);
+    }
+  }
+
+  while (stack.length) {
+    const vertex = stack.pop();
+    count++;
+
+    for (let i = 0; i < prerequisites.length; i++) {
+      if (prerequisites[i][1] == vertex) {
+        graph[prerequisites[i][0]] = graph[prerequisites[i][0]] - 1;
+
+        if (graph[prerequisites[i][0]] == 0) {
+          stack.push(prerequisites[i][0]);
+        }
+      }
+    }
+  }
+
+  return count == numCourses;
+};
