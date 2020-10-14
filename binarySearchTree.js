044cfeea4c49ef17a9a901bfa944246c3e8c6bbf
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2020-10-14 08:31:27 -0400

    feat(binarySearchTree.js): delete node from tree

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -184,3 +184,48 @@ function trimBST(root, low, high) {
 
   return root;
 }
+
+/*
+Delete Node in a BST
+
+Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+*/
+var deleteNode = function (root, key) {
+  if (!root) {
+    return null;
+  }
+
+  if (key > root.val) {
+    root.right = deleteNode(root.right, key);
+  } else if (key < root.val) {
+    root.left = deleteNode(root.left, key);
+  } else {
+    if (!root.left || !root.right) {
+      root = null;
+    } else if (root.right) {
+      root.val = successor(root);
+      root.right = deleteNode(root.right, root.val);
+    } else {
+      root.val = predecessor(root);
+      root.left = deleteNode(root.left, root.val);
+    }
+  }
+  return root;
+};
+
+function predecessor(root) {
+  root = root.left;
+  while (root.right) {
+    root = root.right;
+  }
+  return root.val;
+}
+
+function successor(root) {
+  root = root.right;
+  while (root.left) {
+    root = root.left;
+  }
+  return root.val;
+}
